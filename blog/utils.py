d8da432035e39ca722fefa2f2119ccc832e47dd3
cosven <yinshaowen241@gmail.com>
2018-03-14 21:47:44 +0800

    add highlight and math support

--- blog/utils.py
+++ blog/utils.py
@@ -1,5 +1,15 @@
 import mistune
+from mistune_contrib.highlight import HighlightMixin
+from mistune_contrib.math import MathRendererMixin
+
+
+class MDRenderer(HighlightMixin, MathRendererMixin, mistune.Renderer):
+    pass
+
+
+renderer = MDRenderer()
 
 
 def mdtohtml(text):
-    return mistune.markdown(text)
+    markdown = mistune.Markdown(renderer=renderer)
+    return markdown(text)
